674222900471888490e9404d0b5fd2129ddbabb9
Leonardo Ciccone <Leonardo.Ciccone@swisscom.com>
2025-01-21 17:36:23 +0100

    added skills section

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
-// Canvas-Setup
+
 const canvas = document.getElementById('starCanvas');
 const ctx = canvas.getContext('2d');
 
-// Canvas-Größe anpassen
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Sternen-Array erstellen
-let stars = [];
-const numStars = 600; // Anzahl der Sterne
 
-// Funktion zur Erstellung eines einzelnen Sterns
+let stars = [];
+const numStars = 600; 
+
+
 function createStar() {
     return {
         x: Math.random() * canvas.width,
@@ -61,3 +61,62 @@ window.addEventListener('resize', () => {
 });
 
 animate();
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    function createDonutChart(elementId, percentage, color) {
+        const ctx = document.getElementById(elementId).getContext('2d');
+        
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [percentage, 100 - percentage],
+                    backgroundColor: [color, 'rgba(0,0,0,0)'],
+                    borderWidth: 0
+                }]
+            },
+            options: {
+                responsive: true,
+                cutout: '70%',
+                plugins: {
+                    legend: { display: false }, 
+                    tooltip: { enabled: false },
+                },
+                animation: {
+                    animateRotate: true,
+                    animateScale: true,
+                }
+            },
+            plugins: [{
+                id: 'textPlugin',
+                beforeDraw: function (chart) {
+                    let width = chart.width,
+                        height = chart.height,
+                        ctx = chart.ctx;
+
+                    ctx.restore();
+                    let fontSize = (height / 6).toFixed(2);
+                    ctx.font = fontSize + "px Montserrat";
+                    ctx.textBaseline = "middle";
+                    ctx.textAlign = "center";
+
+                    let text = chart.data.datasets[0].data[0] + "%";
+                    let x = width / 2;
+                    let y = height / 2;
+
+                    ctx.fillStyle = "#ffffff";  // Farbe des Textes
+                    ctx.fillText(text, x, y);
+                    ctx.save();
+                }
+            }]
+        });
+    }
+
+    createDonutChart('htmlChart', 90, 'rgba(235, 111, 54, 0.7)');
+    createDonutChart('cssChart', 80, 'rgba(86, 165, 255, 0.7)');
+    createDonutChart('jsChart', 60, 'rgba(255, 219, 99, 0.7)');
+    createDonutChart('pythonChart', 30, 'rgba(214, 55, 24, 0.7)');
+    createDonutChart('csharpChart', 40, 'rgba(116, 24, 214, 0.7)');
+});
+
